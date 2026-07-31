3761d2221754654b4d234b6724918bbf3a303688
407-30-Treethippayanipa <54677@yupparaj.ac.th>
2026-07-31 11:59:51 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -8,7 +8,7 @@ st.write("กรอกข้อมูลน้ำหนักและส่ว�
 weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):")
 height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):")
 
- if st.button("คำนวณค่า BMI"):
+if st.button("คำนวณค่า BMI"):
     height_m = height_cm / 100
     bmi = weight / (height_m ** 2)
   
